be2554037dde347e1fba3ee7e3978cc755b7a2af
Tom Beynon <tombeynon@gmail.com>
2015-12-20 21:33:51 +0000

    Tests

--- spec/lita/handlers/ai_spec.rb
+++ spec/lita/handlers/ai_spec.rb
@@ -1,4 +1,78 @@
 require "spec_helper"
 
 describe Lita::Handlers::Ai, lita_handler: true do
+  describe 'handling unhandled_message' do 
+    context 'unhandled directed at lita' do
+      it 'uses cleverbot to reply' do
+        allow(subject.class.cleverbot).to receive(:think){ 'Hello' }
+        send_message('Hi lita')
+        expect(replies.last).to eq('Hello')
+      end
+    end
+    context 'unhandled not directed at lita' do
+      it 'doesn\'t reply' do
+        send_message('Not talking to you')
+        expect(replies.last).to be_nil
+      end
+    end
+  end
+
+  describe '#chat' do
+    let(:cleverbot){ double(:cleverbot) }
+    let(:source){ double(:source) }
+    context 'commanding lita' do
+      let(:message){ double(:message, body: 'Hello', command?: true, source: source) } 
+
+      it 'strips out the robot name' do
+        expect(subject.class.cleverbot).to receive(:think).with('Hello')
+        subject.chat(message: message)
+      end
+
+      it 'strips out the robot aliases' do
+        robot.alias = 'bender'
+        allow(message).to receive(:body){ 'Hi bender' }
+        expect(subject.class.cleverbot).to receive(:think).with('Hi')
+        subject.chat(message: message)
+      end
+
+      it 'sends a message with cleverbot\'s response' do
+        reply = double(:reply)
+        expect(subject.class.cleverbot).to receive(:think).with('Hello'){ reply }
+        expect(subject.robot).to receive(:send_message).with(source, reply)
+        subject.chat(message: message)
+      end
+    end
+
+    context 'mentioning lita' do
+      let(:message){ double(:message, body: 'Hi lita', command?: false, source: source) } 
+
+      it 'strips out the robot name' do
+        expect(subject.class.cleverbot).to receive(:think).with('Hi')
+        subject.chat(message: message)
+      end
+
+      it 'strips out the robot aliases' do
+        robot.alias = 'bender'
+        allow(message).to receive(:body){ 'Hello bender' }
+        expect(subject.class.cleverbot).to receive(:think).with('Hello')
+        subject.chat(message: message)
+      end
+
+      it 'sends a message with cleverbot\'s response' do
+        reply = double(:reply)
+        expect(subject.class.cleverbot).to receive(:think).with('Hi'){ reply }
+        expect(subject.robot).to receive(:send_message).with(source, reply)
+        subject.chat(message: message)
+      end
+    end
+
+    context 'is not chatting' do
+      let(:message){ double(:message, body: 'Not talking to you', command?: false, source: source) } 
+
+      it 'Doesn\'t call cleverbot' do
+        expect(subject.class.cleverbot).to_not receive(:think)
+        subject.chat(message: message)
+      end
+    end
+  end
 end
